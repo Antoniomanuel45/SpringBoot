f1e9591f73a3f1f884dde9414ee3f9ebb4eccb8b
Antonio Manuel <antoniomanuelbarrios@gmail.com>
2026-06-23 09:05:52 +0200

    20262306

--- crudalumnos/src/main/java/com/inserta/crudalumnos/controlador/AsignaturaController.java
+++ crudalumnos/src/main/java/com/inserta/crudalumnos/controlador/AsignaturaController.java
@@ -1,43 +1,35 @@
 package com.inserta.crudalumnos.controlador;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.inserta.crudalumnos.modelo.Alumno;
 import com.inserta.crudalumnos.modelo.Asignatura;
-import com.inserta.crudalumnos.repositorio.AlumnoRepository;
 import com.inserta.crudalumnos.repositorio.AsignaturaRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
-
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
-//Para la Api/Rest (en el controlador)se pone la anotacion @Restcontroller
-// @ResquestMapping define el endpoint (la url que sale en swagger/postman) 
+
+// Para la API/Rest (en el controlador) se pone la anotación @RestController
+// @RequestMapping define el endpoint (la URL que sale en swagger/postman)
 @RestController
 @RequestMapping("/api/asignaturas")
-
 public class AsignaturaController {
-    //Agregamos el repo
+    // Agregamos el repo
     private final AsignaturaRepository asignaturaRepo;
 
-     //Constructor del Repo
     public AsignaturaController(AsignaturaRepository asignaturaRepo) {
         this.asignaturaRepo = asignaturaRepo;
     }
 
-    
+    // Por cada endpoint se crea un método
+    // endpoint -> http://localhost:8080/api/asignaturas/consultar
     @GetMapping("/consultar")
     @Operation(summary = "Lista asignaturas")
-    public List <Asignatura> verAsignaturas(){
+    public List<Asignatura> verAsignaturas(){
         return asignaturaRepo.findAll();
     }
-
 
 
 }
